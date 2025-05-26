6d6297ffe901474cc424db16f94af31e00368452
Ajaikrishn <ajaikrishna114@gmail.com>
2025-05-26 16:14:39 +0530

    ui intiated

--- agent/main.py
+++ agent/main.py
@@ -1,4 +1,4 @@
-from agent_maneger import run_assistant,process_user_input
+from agent.agent_manager import run_assistant,process_user_input
 from langchain_ollama import OllamaLLM
 from langchain.prompts import PromptTemplate
 from langchain.chains import LLMChain
@@ -18,18 +18,24 @@ chat_chain = LLMChain(
 
 def handle_input(user_input):
     return process_user_input(user_input)  
+def is_email_query(query):
+    # Basic email intent check
+    email_keywords = ["email", "mail", "inbox", "fetch", "read", "summarize"]
+    return any(word in query.lower() for word in email_keywords)
 
 def main():
-    query = input("Enter your query: ")
-    response = chat_chain.invoke(query)
-    if query.lower() == "exit":
-        print("Exiting the assistant.")
-        return
-    elif query.lower() == "fetch emails":
-        run_assistant()
-    elif query.lower()== query:
-        print(f'ai_response: {response}')
-    
+    while True:
+        query = input("Enter your query: ")
+        response = chat_chain.invoke(query)
+        if query.lower() == "exit":
+            print("Exiting the assistant.")
+            return
+        elif is_email_query(query):
+            print("📬 Fetching and summarizing your emails...")
+            run_assistant()
+        else :
+            print(f"ai_response: {response['text']}") 
+        
    
 
 if __name__ == "__main__":
